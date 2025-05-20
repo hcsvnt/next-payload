91b866e3d09e2f565a48ea1b2e45e39bebfa82a0
Mateusz Hallala <matt@huncwot.com>
2025-05-20 22:52:36 +0200

    feat: chainable middlewares

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,53 +1,79 @@
-import type { NextRequest } from 'next/server';
+import type { NextFetchEvent, NextRequest } from 'next/server';
 
 import { match } from '@formatjs/intl-localematcher';
+import { trace } from '@opentelemetry/api';
 import { pipe } from 'effect';
 import Negotiator from 'negotiator';
 import { NextResponse } from 'next/server';
 
 import { DEFAULT_LOCALE, LOCALES } from '@/static';
 
-export function middleware(request: NextRequest) {
-    // return NextResponse.next(); // bypass middleware
-    return localeMiddleware(request); // use locale middleware
-}
+export type ChainableMiddleware = (
+    request: NextRequest,
+    event: NextFetchEvent
+) => Promise<NextResponse>;
 
-function getLocale(request: NextRequest): string {
-    let locale = '';
+export type MiddlewareFactory = (next: ChainableMiddleware) => ChainableMiddleware;
 
-    try {
-        locale = match(
-            new Negotiator({
-                headers: { 'accept-language': request.headers.get('accept-language') ?? undefined }
-            }).languages(),
-            LOCALES.map(({ code }) => code),
-            DEFAULT_LOCALE.code
-        );
-    } catch (e) {
-        console.error(e);
-        locale = DEFAULT_LOCALE.code;
+export function chainMiddleware(
+    functions: MiddlewareFactory[] = [],
+    index = 0
+): ChainableMiddleware {
+    const current = functions[index];
+    if (current) {
+        const next = chainMiddleware(functions, index + 1);
+        return current(next);
     }
 
-    return locale;
+    return async request => NextResponse.next({ request });
 }
 
-function localeMiddleware(request: NextRequest) {
+// --- Middleware entry point ---
+export function middleware(request: NextRequest, event: NextFetchEvent) {
+    return chainMiddleware([localeMiddleware])(request, event);
+}
+
+export const config = {
+    matcher: [
+        '/((?!_next).*)' // Skip all internal paths (_next)
+    ]
+};
+
+// --- Telemetry middleware ---
+const _telemetryMiddleware: MiddlewareFactory = next => async (request, event) => {
+    const response = await next(request, event);
+    const current = trace.getActiveSpan();
+
+    // set server-timing header with traceparent
+    if (current) {
+        response.headers.set(
+            'server-timing',
+            `traceparent;desc="00-${current.spanContext().traceId}-${current.spanContext().spanId}-01"`
+        );
+    }
+
+    return response;
+};
+
+// --- Locale middleware ---
+const localeMiddleware: MiddlewareFactory = next => async (request, event) => {
     const { pathname } = request.nextUrl;
 
-    const isRootPath = pathname === '/';
-    const isDocsPath = pathname.startsWith('/docs');
+    const _isRootPath = pathname === '/';
+    const _isDocsPath = pathname.startsWith('/docs');
+    const _isNotFoundPath = pathname.startsWith('/404');
     const isPayloadPath = ['/admin', '/api'].some(path => pathname.includes(path));
     const pathnameHasLocale = LOCALES.map(({ code }) => code).some(locale =>
-        pathname.startsWith(`/${locale.toLowerCase()}`)
+        pathname.startsWith(`/${locale}`)
     );
 
-    // Skip locale middleware for root, docs, and payload paths
-    if ([isRootPath, pathnameHasLocale, isDocsPath, isPayloadPath].some(Boolean)) {
-        return NextResponse.next();
+    // Bypass middleware for selected paths
+    if ([pathnameHasLocale, isPayloadPath].some(Boolean)) {
+        return next(request, event);
     }
 
     const cookieLocale = request.cookies.get('locale')?.value;
-    // If browser has a locale cookie, redirect to that locale
+
     if (cookieLocale) {
         const locale = match(
             [cookieLocale],
@@ -57,20 +83,28 @@ function localeMiddleware(request: NextRequest) {
         return redirectToLocale(request, locale);
     }
 
-    // If the pathname doesn't have a locale, redirect to one that matches the
-    // user's accept-language header or the default locale
-    return pipe(request, getLocale, locale => redirectToLocale(request, locale));
+    const locale = pipe(request, getLocale, locale => locale);
+    return redirectToLocale(request, locale);
+};
+
+// --- Helper functions ---
+function getLocale(request: NextRequest): string {
+    try {
+        return match(
+            new Negotiator({
+                headers: { 'accept-language': request.headers.get('accept-language') ?? undefined }
+            }).languages(),
+            LOCALES.map(({ code }) => code),
+            DEFAULT_LOCALE.code
+        );
+    } catch (e) {
+        console.error(e);
+        return DEFAULT_LOCALE.code;
+    }
 }
 
 const redirectToLocale = (request: NextRequest, locale: string) => {
-    request.nextUrl.pathname = `/${locale.toLowerCase()}${request.nextUrl.pathname}`;
-    return NextResponse.redirect(request.nextUrl);
-};
-
-export const config = {
-    matcher: [
-        '/((?!_next).*)' // Skip all internal paths (_next)
-        // '/', // Match the root
-        // `/(${LOCALES.map(({ code }) => code.toLowerCase()).join('|')})/:path*` // Match only internationalized pathnames
-    ]
+    const url = request.nextUrl.clone();
+    url.pathname = `/${locale}${request.nextUrl.pathname}`;
+    return NextResponse.redirect(url);
 };
