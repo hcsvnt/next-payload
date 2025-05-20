7025693e5fed980509a0d5f1c1e286f2441e9d3c
Mateusz Hallala <matt@huncwot.com>
2025-05-20 23:13:13 +0200

    feat: page transition; chore: plugins

---
+++ src/app/(site)/route-transition.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+// import { wait } from '@/utils/wait';
+import { AnimatePresence, motion } from 'framer-motion';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+
+import '@/styles/main.scss';
+import { easings } from '@/animations';
+
+/**
+ * In order to achieve a page exit transition we need to move the
+ * AnimatePresence, but also the motion element into layout as it otherwise will
+ * not work (the standard is to deal with all this inside the template file
+ * which is the next layer within a layout, but it does not persist across route
+ * changes).
+ * It does not have any impact on the main content as it is passed as children
+ * meaning they can still be server only components.
+ *
+ * To prevent rerendering on some paths, use just a part of the path e.g. for
+ * /blog/posts/1 use /blog/posts.
+ */
+
+export default function RouteTransition({ children }: { children: React.ReactNode }) {
+    const pathname = usePathname();
+
+    return (
+        <AnimatePresence initial={false}>
+            <motion.div
+                key={pathname}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{
+                    duration: 0.6,
+                    ease: easings['ease-in-out']
+                }}
+                onAnimationStart={() => {
+                    // here we can handle a curtain for example
+                    // wait(400).then(() => {
+                    //     document.body.classList.remove('is-navigating');
+                    //     document.body.classList.add('is-navigated');
+                    // });
+                }}
+            >
+                {children}
+            </motion.div>
+        </AnimatePresence>
+    );
+}
--- src/payload.plugins.ts
+++ src/payload.plugins.ts
@@ -1,14 +1,11 @@
 // import { redirectsPlugin } from '@payloadcms/plugin-redirects';
-import { seoPlugin } from '@payloadcms/plugin-seo';
-// import { s3Storage } from '@payloadcms/storage-s3';
+// import { seoPlugin } from '@payloadcms/plugin-seo';
 import { uploadthingStorage } from '@payloadcms/storage-uploadthing';
 
 /**
  * Plugins:
- * - payloadCloudPlugin: Enables Payload Cloud
- * - redirectsPlugin: Enables Redirects
- * - seoPlugin: Adds SEO fields to collections
- * - s3Storage: Enables S3 Storage
+ * - TODO redirectsPlugin: Enables Redirects
+ * - TODO seoPlugin: Adds SEO fields to collections
  * - uploadthingStorage: Enables UploadThing Storage
  */
 
@@ -21,40 +18,18 @@ export const plugins = [
             token: process.env.UPLOADTHING_TOKEN,
             acl: 'public-read'
         }
-    }),
-    seoPlugin({
-        collections: ['pages'],
-        uploadsCollection: 'images',
-        generateTitle: ({ doc }) => `${process.env.SEO_TITLE} — ${doc.title}`,
-        generateDescription: ({ doc }) => doc.title
-        // fields: ({ defaultFields }) => [
-        //     ...defaultFields,
-        //     {
-        //         name: 'customField',
-        //         type: 'text'
-        //     }
-        // ]
     })
+    // seoPlugin({
+    //     collections: ['pages'],
+    //     uploadsCollection: 'images',
+    //     generateTitle: ({ doc }) => `${process.env.SEO_TITLE} — ${doc.title}`,
+    //     generateDescription: ({ doc }) => doc.title
+    //     // fields: ({ defaultFields }) => [
+    //     //     ...defaultFields,
+    //     //     {
+    //     //         name: 'customField',
+    //     //         type: 'text'
+    //     //     }
+    //     // ]
+    // })
 ];
-
-// todo
-
-// // todo: this doesn't seem to catch the redirects
-// redirectsPlugin({
-//     collections: ['pages'],
-//     redirectTypes: ['301', '302']
-// }),
-
-// s3Storage({
-//     collections: {
-//         media: true
-//     },
-//     bucket: process.env.S3_BUCKET || 'OOPS!',
-//     config: {
-//         credentials: {
-//             accessKeyId: process.env.S3_ACCESS_KEY_ID || 'OOPS!',
-//             secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || 'OOPS!'
-//         },
-//         region: process.env.S3_REGION
-//     }
-// }),
