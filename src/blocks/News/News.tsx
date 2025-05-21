8970986eaeb78c76f76925a25f32b4356d31bdb2
Mateusz Hallala <matt@huncwot.com>
2025-05-21 12:02:34 +0200

    refactor: news type casting

--- src/blocks/News/News.tsx
+++ src/blocks/News/News.tsx
@@ -2,28 +2,31 @@ import { Heading } from '@radix-ui/themes';
 
 import type { News, NewsBlock } from '@/payload.types';
 
+import { isIdNotContent } from '@/components/NotContent';
 import RichText from '@/components/RichText/RichText';
 
 import styles from './News.module.scss';
 
 export default async function News({ title, news }: NewsBlock) {
-    if (!news?.length) return <p>No news to display</p>;
-
     return (
         <div className={styles.container}>
             <p>{title}</p>
             <ul>
-                {/* todo: shouldn't be casted */}
-                {(news as News[]).map(({ id, title, content }) => {
-                    return (
-                        <li key={id}>
-                            <Heading as='h2'>{title}</Heading>
-                            {/* <Text>{category.title}</Text> */}
-                            {content && <RichText data={content} />}
-                        </li>
-                    );
-                })}
+                {news
+                    .filter((newsItem): newsItem is News => !isIdNotContent(newsItem))
+                    .map(props => (
+                        <NewsItem key={props.id} {...props} />
+                    ))}
             </ul>
         </div>
+    );
+}
+
+function NewsItem({ title, content }: News) {
+    return (
+        <li>
+            <Heading as='h2'>{title}</Heading>
+            {content && <RichText data={content} />}
+        </li>
     );
 }
